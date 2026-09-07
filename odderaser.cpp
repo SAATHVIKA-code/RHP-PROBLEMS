a0d9bfe56fae1a379622091bddb73fc3b101f86e
SAATHVIKA-code <25ee045@rmkec.ac.in>
2026-09-07 09:39:32 +0530

    Add HCF calculation for first and last array elements
    Implement a program to calculate the HCF of the first and last elements of an input array.

---
+++ odderaser.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<queue>
+using namespace std;
+int hcf(int a,int b){
+  return b==0?a:hcf(b,a%b);
+}
+void solve(){
+  int N;
+cin>>N;
+vector<int> arr(N);
+for(int i=0;i<N;i++){
+cin>>arr[i];
+}
+cout<<hcf(arr[0],arr[N-1])<<endl;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc=1;
+    while(tc--){
+    	solve();
+    }
+
+
+    return 0;
+}
+  
